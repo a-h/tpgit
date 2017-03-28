037bf20be3e694b304578bf9b90c5a580fcbdfb2
Adrian Hesketh <adrianhesketh@hushmail.com>
2017-03-28 16:44:16 +0100

    Switched to JSON logging and added locking.

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"flag"
+	"io"
 	"os"
 	"regexp"
 
 	"strconv"
 
-	"fmt"
+	"strings"
 
+	"bufio"
+
+	"io/ioutil"
+
+	log "github.com/Sirupsen/logrus"
 	"github.com/a-h/tpgit/git"
 )
 
@@ -16,24 +22,90 @@ var repo = flag.String("repo", "https://github.com/a-h/ver", "The repo to query 
 var dryRun = flag.Bool("dryRun", true, "Set to true (default) to see what changes would be made.")
 
 func main() {
+	os.Exit(run())
+}
+
+func run() int {
 	flag.Parse()
 
+	log.SetFormatter(&log.JSONFormatter{})
+	logger := log.WithField("repo", *repo)
+
+	fn := getFileNameFromRepo(*repo)
+	lockfileName := "." + fn
+	if _, err := os.Stat(lockfileName); !os.IsNotExist(err) {
+		logger.Errorf("lock file %v is present for repo %v. not starting.\n", lockfileName, *repo)
+		return -1
+	}
+	err := ioutil.WriteFile(lockfileName, []byte{}, 0666)
+	defer os.Remove(lockfileName)
+
+	previousHashes, err := loadHashes(fn)
+	if !os.IsNotExist(err) {
+		logger.Errorf("failed to load previous hashes from file %v with error: %v\n", fn, err)
+		return -1
+	}
+
+	logger.Info("cloning repo")
 	r, err := git.Clone(*repo)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to clone the repo: %v\n", err)
-		os.Exit(-1)
+		logger.Errorf("failed to clone the repo: %v\n", err)
+		return -1
 	}
 
-	log, err := r.Log()
+	logger.Info("getting commit log from repo")
+	commitlog, err := r.Log()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to get the log: %v\n", err)
-		os.Exit(-1)
+		logger.Errorf("failed to get the commit log: %v\n", err)
+		return -1
 	}
 
-	for _, entry := range log {
-		fmt.Printf("%d - %s\n", extract(entry.Body), entry.Body)
-		fmt.Println("----------------------------------------------")
+	for _, entry := range commitlog {
+		entryLogger := logger.WithField("hash", entry.Hash)
+
+		if _, ok := previousHashes[entry.Hash]; ok {
+			entryLogger.Info("skipping already processed hash")
+			continue
+		}
+		entryLogger = entryLogger.WithField("git_timestamp", entry.Timestamp)
+		entryLogger = entryLogger.WithField("name", entry.Name)
+		entryLogger = entryLogger.WithField("ids", extract(entry.Body))
+		entryLogger = entryLogger.WithField("body", entry.Body)
+		entryLogger.Info("processing commit")
 	}
+
+	return 0
+}
+
+func getFileNameFromRepo(repo string) string {
+	return strings.NewReplacer("/", "-", ":", "-", ".", "-").Replace(repo)
+}
+
+func loadHashes(fileName string) (map[string]bool, error) {
+	op := make(map[string]bool)
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return op, err
+	}
+	defer file.Close()
+
+	r := bufio.NewReader(file)
+
+	var line string
+	for {
+		line, err = r.ReadString('\n')
+		op[strings.TrimSpace(line)] = true
+		if err != nil {
+			break
+		}
+	}
+
+	if err != io.EOF {
+		return op, err
+	}
+
+	return op, nil
 }
 
 var re = regexp.MustCompile(`(?i)(?:(?:TP)?(?:\-|\s+|\:|^#))(?P<id>\d+)`)
